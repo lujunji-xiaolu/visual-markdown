143aee4a493f84527f7470a6d5eed332428bf2c9
lujunji <lujunji-xiaolu@outlook.com>
2022-07-29 22:53:29 +0800

    Window vibrancy

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,34 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
+use tauri::Manager;
+use window_vibrancy::apply_mica;
 
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .setup(|app| {
+            let window = app.get_window("main").unwrap();
+
+            #[cfg(target_os = "windows")]
+            apply_mica(&window)
+                .expect("Unsupported platform! 'apply_mica' is only supported on Windows 11");
+
+            #[cfg(debug_assertions)] // only include this code on debug builds
+            {
+                window.open_devtools();
+            }
+
+            // Mica Background in Tauri JS https://github.com/tauri-apps/window-vibrancy/issues/52
+            window.minimize().unwrap();
+            window.unminimize().unwrap();
+
+            window.show().unwrap();
+
+            window.set_focus().unwrap();
+
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
